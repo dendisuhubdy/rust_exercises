af58228c5cd4ecd877445c19f17b6d582d5bac2d
Dendi Suhubdy <dendi@bitwyre.com>
2022-10-11 09:44:13 +0800

    fix: integration testing call binary

--- integration_testing/tests/cli.rs
+++ integration_testing/tests/cli.rs
@@ -1,4 +1,13 @@
+use std::process::Command;
+
 #[test]
 fn works() {
     assert!(true);
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::new("./target/debug/integration_testing");
+    let res = cmd.output();
+    assert!(res.is_ok());
 }
